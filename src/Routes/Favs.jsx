47f704b0def7473dbd61d7c40901472d63d62d52
ricardocuellar <rcuellarsn@gmail.com>
2024-12-03 21:37:01 -0600

    Add favorites in favs page

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -4,13 +4,18 @@ import Card from "../Components/Card";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
-
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
   return (
     <>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
         {/* este componente debe consumir los destacados del localStorage */}
         {/* Deberan renderizar una Card por cada uno de ellos */}
+
+        {favorites.map((favorite) => (
+          <Card name={favorite.name} username={favorite.username} id={favorite.id} />
+        ))}
+
       </div>
     </>
   );
