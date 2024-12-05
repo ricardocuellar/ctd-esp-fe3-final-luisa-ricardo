8453c994138df31b81788c3bf3975540a8f89e9e
LuisaVargas-9 <luisavr0997@gmail.com>
2024-12-05 01:03:05 -0300

    creation of layout route

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,20 @@ import Home from "./Routes/Home";
 import Favs from "./Routes/Favs";
 import Detail from "./Routes/Detail";
 import Contact from "./Routes/Contact";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
+import Layout from "./Layouts/Layout";
 
 
 function App() {
   return (
       <>
-        <Navbar />
-
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/favs" element={<Favs/>}/>
-          <Route path="/detail/:id" element={<Detail/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/favs" element={<Favs/>}/>
+            <Route path="/detail/:id" element={<Detail/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Route>
         </Routes>
-
-        <Footer/>
       </>
   );
 }
